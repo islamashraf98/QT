bc798fbb63c1cc405ae5c5a4e1594123af3976ec
Islam <islamashraf710@yahoo.com>
2021-12-19 12:26:41 +0200

    Class creation

--- try.py
+++ try.py
@@ -1,20 +1,55 @@
+from PyQt5.QtWidgets import QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout
+from PyQt5.QtGui import QIcon
 import sys
-from PyQt5.QtWidgets import QVBoxLayout, QComboBox, QApplication, QWidget, QLabel
 
-class Window():
-    app = QApplication(sys.argv)
-    widget = QWidget()
-    textlabel = QLabel(widget)
-    combobox = QComboBox()
-    combobox.addItem('one')
-    combobox.addItem('two')
-    layout = QVBoxLayout()
-    layout.addWidget(combobox)
-    widget.setGeometry(400, 400, 400, 200)
-    widget.setWindowTitle("First PyQt5")
-    widget.setLayout(layout)
-    widget.show()
-    sys.exit(app.exec_())
 
-if __name__ == '__main__':
-    window()
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.showMaximized()
+        combobox1 = QComboBox()
+        combobox1.addItem('One')
+        combobox1.addItem('Two')
+        combobox1.addItem('Three')
+        combobox1.addItem('Four')
+
+        combobox2 = QComboBox()
+        combobox2.addItems(['One', 'Two', 'Three', 'Four'])
+
+        combobox3 = QComboBox()
+        combobox3.addItems(['One', 'Two', 'Three', 'Four'])
+        combobox3.insertItem(2, 'Hello!')
+
+        combobox4 = QComboBox()
+        combobox4.addItems(['One', 'Two', 'Three', 'Four'])
+        combobox4.insertItems(2, ['Hello!', 'again'])
+
+        combobox5 = QComboBox()
+        icon_penguin = QIcon('animal-penguin.png')
+        icon_monkey = QIcon('animal-monkey.png')
+        icon_bauble = QIcon('bauble.png')
+        combobox5.addItem(icon_penguin, 'Linux')
+        combobox5.addItem(icon_monkey, 'Monkeyix')
+        combobox5.insertItem(1, icon_bauble, 'Baublix')
+
+        layout = QVBoxLayout()
+        layout.addWidget(combobox1)
+        layout.addWidget(combobox2)
+        layout.addWidget(combobox3)
+        layout.addWidget(combobox4)
+        layout.addWidget(combobox5)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def current_text_changed(self, s):
+        print("Current text: ", s)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec_()
