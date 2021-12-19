6d2814af0699b696e6d734791145651ff24160b1
Islam <islamashraf710@yahoo.com>
2021-12-19 16:50:21 +0200

    Receiving done

--- receive.py
+++ receive.py
@@ -6,10 +6,8 @@ port = 8000                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 while True:
-   print("Hello") 
    c, addr = s.accept()     # Establish connection with client.
-   print("Hello2")
-   data = s.recv(1024)
+   data = c.recv(1024)
    print ('Got connection from', addr)
    print (data)
    c.close()                # Close the connection
