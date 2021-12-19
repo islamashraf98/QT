a9271e66be46360b65d40178147f39f62cec2c7c
omarhaassaan <omarhassan321@hotmail.com>
2021-12-19 15:24:27 +0200

    ComboBox

--- try.py
+++ try.py
@@ -1,52 +1,41 @@
-from PyQt5.QtWidgets import QComboBox, QMainWindow, QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 import sys
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
-        super().__init__()
-        self.showMaximized()
-        combobox1 = QComboBox()
-        combobox1.addItem('One')
-        combobox1.addItem('Two')
-        combobox1.addItem('Three')
-        combobox1.addItem('Four')
-
-        combobox2 = QComboBox()
-        combobox2.addItems(['One', 'Two', 'Three', 'Four'])
-
-        combobox3 = QComboBox()
-        combobox3.addItems(['One', 'Two', 'Three', 'Four'])
-        combobox3.insertItem(2, 'Hello!')
-
-        combobox4 = QComboBox()
-        combobox4.addItems(['One', 'Two', 'Three', 'Four'])
-        combobox4.insertItems(2, ['Hello!', 'again'])
-
-        combobox5 = QComboBox()
-        icon_penguin = QIcon('animal-penguin.png')
-        icon_monkey = QIcon('animal-monkey.png')
-        icon_bauble = QIcon('bauble.png')
-        combobox5.addItem(icon_penguin, 'Linux')
-        combobox5.addItem(icon_monkey, 'Monkeyix')
-        combobox5.insertItem(1, icon_bauble, 'Baublix')
-
-        layout = QVBoxLayout()
-        layout.addWidget(combobox1)
-        layout.addWidget(combobox2)
-        layout.addWidget(combobox3)
-        layout.addWidget(combobox4)
-        layout.addWidget(combobox5)
-
+        super().__init__(None)
+        self.layout = QVBoxLayout(self)
+        self.line_edit = QLineEdit()
+        self.line_edit.setPlaceholderText('Enter a color')
+        self.line_edit.setStyleSheet('QLineEdit {background-color:white}')
+        self.layout.addWidget(self.line_edit)
+        self.line_edit.textEdited.connect(self.sensors)
         container = QWidget()
-        container.setLayout(layout)
-
+        container.setLayout(self.layout)
         self.setCentralWidget(container)
 
-    def current_text_changed(self, s):
-        print("Current text: ", s)
+    # def change_background(self):
+    #     col = self.line_edit.text()
+    #     self.setStyleSheet(f'QWidget {{background-color: {col};}}')
+    #     self.line_edit.clear()
+
+    def sensors(self):
+        for i in range(int(self.line_edit.text())):
+            print(i)
+            hlayout = QHBoxLayout()
+
+            combobox1 = QComboBox()
+            combobox1.addItems(['zeby'])
+
+            combobox2 = QComboBox()
+            combobox2.addItems(['omar'])
+
+            hlayout.addWidget(combobox1)
+            hlayout.addWidget(combobox2)
+            self.layout.addLayout(hlayout)
+
 
 
 app = QApplication(sys.argv)
